399e6916e1c73c246c175a083d5fe4d4369b7c5f
Расул Хияров <hiyarovrasul07@gmail.com>
2024-01-31 22:58:36 +0600

    hw4

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 
-
 class Director(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
-
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
@@ -17,10 +15,9 @@ class Movie(models.Model):
     def __str__(self):
         return self.title
 
-
 class Review(models.Model):
     text = models.TextField()
-    stars = models.IntegerField(default=1, choices=[(i, i * '*') for i in range(1, 6)])
+    stars = models.CharField(max_length=1, choices=[('1', '*'), ('2', '**'), ('3', '***'), ('4', '****'), ('5', '*****')], default='1')
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='reviews')
 
     def __str__(self):
--- movie_app/serializers.py
+++ movie_app/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from .models import Director, Movie, Review
 
+def validate_name_length(value, min_length):
+    if len(value) < min_length:
+        raise serializers.ValidationError(f'минимальное количество букв {min_length}')
 
 class DirectorSerializer(serializers.ModelSerializer):
     movie_count = serializers.SerializerMethodField()
@@ -12,18 +15,23 @@ class DirectorSerializer(serializers.ModelSerializer):
     def get_movie_count(self, obj):
         return obj.movies.count()
 
+    def validate_name(self, value):
+        validate_name_length(value, 3)
+        return value
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
 
-
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
 
+    def validate_text(self, value):
+        validate_name_length(value, 3)
+        return value
 
 class MovieReviewSerializer(serializers.ModelSerializer):
     reviews = ReviewSerializer(many=True)
@@ -36,7 +44,15 @@ class MovieReviewSerializer(serializers.ModelSerializer):
     def get_average_rating(self, obj):
         total_stars = sum(review.stars for review in obj.reviews.all())
         num_reviews = obj.reviews.count()
-        if num_reviews > 0 :
+        if num_reviews > 0:
             return total_stars / num_reviews
         else:
             return 0.0
+
+    def validate_name(self, value):
+        validate_name_length(value, 3)
+        return value
+
+
+
+
